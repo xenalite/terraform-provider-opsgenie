d4a067c076be5b4484f3b72210d1a29d7b12616a
oskar.mamrzynski <oskar.mamrzynski@wintechnologies.net>
2020-06-23 07:52:45 +0100

    Changed default log level of logger passed to Opsgenie Go SDK

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/hashicorp/terraform-plugin-sdk v1.0.0
 	github.com/opsgenie/opsgenie-go-sdk-v2 v1.0.7-0.20200406122459-ce3f70f22ab7
 	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.2
 )
--- opsgenie/config.go
+++ opsgenie/config.go
@@ -2,9 +2,10 @@ package opsgenie
 
 import (
 	"log"
-
+  "time"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/opsgenie/opsgenie-go-sdk-v2/client"
+	"github.com/sirupsen/logrus"
 )
 
 type OpsgenieClient struct {
@@ -17,11 +18,19 @@ type Config struct {
 }
 
 func (c *Config) Client() (*OpsgenieClient, error) {
+	myLogger := logrus.New()
+	myLogger.SetLevel(logrus.WarnLevel)
+	myLogger.SetFormatter(&logrus.JSONFormatter{
+			TimestampFormat:  time.RFC3339Nano,
+			PrettyPrint:      true,
+	},)
+
 	config := &client.Config{
 		ApiKey:         c.ApiKey,
 		RetryCount:     10,
 		OpsGenieAPIURL: client.ApiUrl(c.ApiUrl),
 		Backoff:        retryablehttp.DefaultBackoff,
+		Logger: myLogger,
 	}
 	ogCli, err := client.NewOpsGenieClient(config)
 	if err != nil {
